9106da19b30c299f8ab85cc91aab506d7701b03a
Jessi <jessi@Fenando-6.local>
2021-01-11 21:08:59 -0600

    Initial commit

---
+++ Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Jessi on 12/14/20.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Pretitions.swift
@@ -0,0 +1,12 @@
+//
+//  Pretitions.swift
+//  Project7
+//
+//  Created by Jessi on 12/14/20.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
